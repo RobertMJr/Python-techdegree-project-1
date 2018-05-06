7cccff18223d0928ab3f1ea4479a7cf068d51657
RobertMJr <murarurobertionut@yahoo.ro>
2018-05-06 13:03:55 +0300

    Added comments

--- league_builder.py
+++ league_builder.py
@@ -1,19 +1,26 @@
 import csv
-   
+
+#create lists to store future teams and players   
 xp_players = []
 new_players = []
 sharks = []
 dragons = []
 raptors= []
 
+
 def create_teams():
+    # read the csv file
     with open('soccer_players.csv', newline='') as csvfile:
         teamreader = csv.DictReader(csvfile)
+        
+        # sort the teams by experience, dividing them in experienced and unexperienced players
         for row in teamreader:
             if row['Soccer Experience'] == 'YES':
                 xp_players.append((row['Name'], row['Soccer Experience'], row['Guardian Name(s)']))
             else:
                 new_players.append((row['Name'], row['Soccer Experience'], row['Guardian Name(s)']))
+
+    # evenly distribute the experienced players among the three teams            
     for counter, player in enumerate(xp_players):
         if counter <3:
             sharks.append(player)
@@ -21,7 +28,7 @@ def create_teams():
             dragons.append(player)
         else:
             raptors.append(player)
-                
+    # evenly distribute the players with no experience among the three teams            
     for counter, player in enumerate(new_players):
         if counter <3:
             sharks.append(player)
@@ -31,16 +38,21 @@ def create_teams():
             raptors.append(player)
 
 def write_to_file():
+    
+    #open the file teams.txt for writing, create it if it doesn't exist
     file = open("teams.txt", "w")
-        
+
+    # write the details of the Sharks team to the file    
     file.write("Sharks \n" + "====== \n")
     for i in sharks:
         file.write(", ".join(i) + "\n")
-            
+        
+    # write the details of the Dragons team to the file           
     file.write("\nDragons \n" + "======= \n")
     for i in dragons:
         file.write(", ".join(i) + "\n")
-            
+        
+    # write the details of the Raptors team to the file           
     file.write("\nRaptors \n" + "======= \n")
     for i in raptors:
         file.write(", ".join(i) + "\n")
@@ -49,7 +61,7 @@ def write_to_file():
     file.close()
              
 
-       
+# ensure script does not execute when imported       
 if __name__ == "__main__":
     create_teams()
     write_to_file()
